face53e4758df37b3521e9ebc4e2f6efa0c9c96f
ravendasun <122367717+ravendasun@users.noreply.github.com>
2023-10-16 06:19:08 +0530

    Slider added

--- script.js
+++ script.js
@@ -5,4 +5,5 @@
 
 window.addEventListener("scroll",()=>{
     document.getElementById("background").style.backgroundSize = `${window.scrollY * 1 + window.innerWidth}px`;
+    document.getElementById("container").style.transform = `translateY(-${window.scrollY*2}px)`;
 })
